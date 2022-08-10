d07337361f55ed2c75e6e2f6e17b3cafb84df5f2
Iusupovds <105906850+Iusupovds@users.noreply.github.com>
2022-08-10 20:52:22 +0300

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,11 @@
+TOKEN = '5593276120:AAEFzAOyDKeCdanV2ukuZApIMmk7xj7rFqo'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'фунт': 'GBP',
+    'юань': 'CNY',
+    'йена': 'JPY',
+    'рубль': 'RUB',
+
+}
